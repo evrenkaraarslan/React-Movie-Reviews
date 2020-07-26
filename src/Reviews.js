e196a53ec02f0d9c25f6ed84a7fa4e905496b574
evrenkaraarslan <evrenkaraarslan@hotmail.com>
2020-07-26 21:04:56 +0200

    Updates

--- src/Reviews.js
+++ src/Reviews.js
@@ -3,31 +3,27 @@ import {Button} from 'react-bootstrap';
 import './Reviews.css';
 export default function Reviews({title,movies,pic,short,pub,imm,headline}) {
     return (
-        <>
+    <>
         <div className="flexbox-container">
-            <div className="flexbox-item" style={{backgroundColor:'wheat'}}>
-                <h3 style={{color:'red'}}>{movies}</h3>
-                <h4>{short}</h4>
-                <Button href={title} style={{paddingBottom:'10px'}} >Go to Review of {movies}</Button>
+            <div className="flexbox-item" style={{backgroundColor:'rgba(0, 0, 0, 0.226)'}}>
+                <h3 style={{color:'black'}}>{movies}</h3>
+                <h4 style={{color:'white'}}>{short}</h4>
+                <Button href={title} style={{paddingBottom:'10px',color:'yellow',fontSize:'1.3em'}} >Go to Review of {movies}</Button>
 
-                <p>Publication Date {pub}</p>
-              {/*  {(imm) ?
-                <img src={imm} alt=" " style={{borderRadius:'2em'}}/> : <p> </p>} */}
-
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-    {(imm) ?
-                <img src={imm} alt=" " style={{borderRadius:'2em'}}/> : <p> </p>}
-    </div>
-    <div class="flip-card-back">
-      <h1>{movies}</h1>
-      <p>{headline}</p>
-    </div>
-  </div>
-</div>
-            </div> 
-        </div>
-         </>
+                <p style={{color:'white'}}>Publication Date {pub}</p>
+               <div class="flip-card">
+                 <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    {(imm) ?  <img src={imm} alt=" " style={{borderRadius:'2em'}}/> : <p> </p>}
+                  </div>
+                  <div class="flip-card-back">
+                    <h1>{movies}</h1>
+                    <p>{headline}</p>
+                  </div>
+                 </div>
+               </div>
+          </div> 
+      </div>
+    </>
     )
 }
